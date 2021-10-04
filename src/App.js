bd8f7a917eab211f46d52a407069ef5452a03950
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-10-04 23:31:28 +0200

    Product service

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from "./header/Header";
 import ProductList from "./product-list/ProductList";
-import {PRODUCTS} from "./mock-products";
+import {list} from "./product-service";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <ProductList products={PRODUCTS}/>
+      <ProductList products={list()}/>
     </div>
   );
 }
---
+++ src/product-service.js
@@ -0,0 +1,6 @@
+import {PRODUCTS} from "./mock-products";
+
+export const list = () => {
+  console.log('listing products')
+  return PRODUCTS;
+};
